e47a14ad9b828aa59df257fc5ffc62add5a2d31a
Huu Huy <s3817719@rmit.edu.vn>
2021-07-29 16:53:50 +0700

    cart layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { useEffect, useState } from 'react'
 
-import { Navbar, Products } from './components'
+import { Cart, Navbar, Products } from './components'
+
 import { commerce } from './lib/commerce'
 
 
 const App = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         setProducts(data)
     }
 
-    useEffect(() => {
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
 
+    const handleAddtoCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity)
+
+        setCart(item.cart)
+    }
+
+    useEffect(() => {
+        fetchCart()
         fetchProducts()
     }, [])
 
-    console.log(products)
-
     return (
         <div>
-            <Navbar />
-            <Products products={products} />
+            <Navbar totalItems={cart.total_items} />
+            {/* <Products products={products} onAddToCart={handleAddtoCart} /> */}
+            <Cart cart={cart} />
         </div>
     )
 }

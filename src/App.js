904c63efe99b41b84c75472fa49d1c32f146b912
Huu Huy <s3817719@rmit.edu.vn>
2021-07-29 15:04:45 +0700

    product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import Products from './components/Products/Products'
+import React, { useEffect, useState } from 'react'
+
+import { Navbar, Products } from './components'
+import { commerce } from './lib/commerce'
+
 
 const App = () => {
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data)
+    }
+
+    useEffect(() => {
+
+        fetchProducts()
+    }, [])
+
+    console.log(products)
+
     return (
         <div>
-            <Products />
+            <Navbar />
+            <Products products={products} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,15 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
 import Product from './Product/Product'
+import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: "https://media.gucci.com/style/DarkGray_Center_0_0_600x314/1538553607/552089_A9L00_9522_001_100_0000_Light.jpg" },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: "https://cdn.tgdd.vn/Products/Images/44/231246/apple-macbook-air-2020-mgnd3saa-600x600.jpg" }
-]
 
-const Products = props => {
+
+const Products = ({ products }) => {
+    const classes = useStyles()
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
